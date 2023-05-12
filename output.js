a44c337bfde70ac92f6e613b1d3c19ff743da147
VaiDik-cyber <patelvaidik56@gmail.com>
2023-05-12 10:47:17 +0530

    .

--- output.js
+++ output.js
@@ -1,20 +1,21 @@
 // alert("Bhai value dalo");
 
 
-// let age = prompt("Enter your age",22);
-// if(age != null){
-//     document.write(`Your age is ${age}`);
-// }else{
-//     document.write("Age field is blank.");
-// }
-
-
-let response = confirm("Are You Sure, You Want To Delete?");
-if(response){
-    console.log("Deleted");
+let age = prompt("Enter your age",22);
+console.log(typeof(age));
+if(age != null){
+    document.write(`Your age is ${age}`);
 }else{
-    document.write("Not Deleted");
+    document.write("Age field is blank.");
 }
+
+
+// let response = confirm("Are You Sure, You Want To Delete?");
+// if(response){
+//     console.log("Deleted");
+// }else{
+//     document.write("Not Deleted");
+// }
 
 // let type = 5 * 'yes';
 // console.log(type);
